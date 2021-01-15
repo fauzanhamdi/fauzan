6fbc8108bf01f78fa9ff03f4d3233486c250bdf8
fauzan <fourzan_1995@yahoo.com>
2021-01-15 11:39:51 +0800

    Preparefordeploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAX0rGC66R6OUF-E_-PJdVRWGk5801vtYs",
+    authDomain: "fauzanportfolio.firebaseapp.com",
+    projectId: "fauzanportfolio",
+    storageBucket: "fauzanportfolio.appspot.com",
+    messagingSenderId: "544921501155",
+    appId: "1:544921501155:web:ca23e20be7d595ac1af4d6",
+    measurementId: "G-8FSMEZF05P"
+  }
 };
